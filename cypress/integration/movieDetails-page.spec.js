fd4f3480d70b3ea97708fb36c7d62978676ce0e6
Xianping Tao <20091578@mail.wit.ie>
2020-11-04 00:46:36 +0000

    E2E tests - Movie Details page - poster image

--- cypress/integration/movieDetails-page.spec.js
+++ cypress/integration/movieDetails-page.spec.js
@@ -52,4 +52,10 @@ describe("Movie Details Page", () => {
       .should("have.attr", "href")
       .should("include", movie.homepage);
   });
+  it("should display the poster image", () => {
+    cy.get("img")
+      .should("have.attr", "src")
+      .should("include", movie.poster_path);
+  });
+
 });
