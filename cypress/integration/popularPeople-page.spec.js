d3067d190bfee8ecdab57e8749eef3bbb74fd0b4
Xianping Tao <20091578@mail.wit.ie>
2020-12-14 23:05:40 +0000

    E2E tests - PopularPeople page

---
+++ cypress/integration/popularPeople-page.spec.js
@@ -0,0 +1,29 @@
+let people;   
+
+
+
+  describe("PopularPeople Page ", () => {
+    before(() => {
+      // Get movies from TMDB and store in movies variable.
+      cy.request(
+        `https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env(
+          "TMDB_KEY"
+        )}&language=en-US&include_adult=false&include_video=false&page=1`
+      )
+        .its("body")    // Take the body of HTTP response from TMDB
+        .then((response) => {
+          people = response.results
+        })
+    })
+
+    beforeEach(() => {
+      cy.visit("/people");
+    });
+  
+    describe("Base test", () => {
+      it("displays page header", () => {
+        cy.get("h2").contains("Popular People");
+        cy.get(".badge").contains(20);
+      });
+    })
+})
